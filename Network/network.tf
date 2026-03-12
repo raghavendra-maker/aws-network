07de62f3b30087a41a6fd3615c92c6cf2c69d080
raghavendra <raghavendra2555@gmail.com>
2026-03-12 14:12:23 +0530

    modified

---
+++ Network/network.tf
@@ -0,0 +1,11 @@
+module "network" {
+  source  = "app.terraform.io/test_pol/network/aws"
+  version = "1.0.0"
+  vpc_cidr            = "172.16.0.0/16"
+  vpc_name            = "CloudGroup"
+  env                 = "Dev"
+  public_subnet_cidr  = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  private_subnet_cidr = null
+  availability_zone   = ["us-east-2a", "us-east-2b", "us-east-2c"]
+NGW=null
+}
